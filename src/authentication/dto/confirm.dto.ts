bd514406c6197e1dd712a85c4790fbe6f90a053a
Ilya Stralkouski <i.stralkouski@a1.by>
2023-10-28 00:33:03 +0300

    replace validation code from @IsString() to @IsNumberString()

--- src/authentication/dto/confirm.dto.ts
+++ src/authentication/dto/confirm.dto.ts
@@ -1,14 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, Length } from 'class-validator';
+import { IsEmail, Length, IsNumberString } from 'class-validator';
 import { EXAMPLE_EMAIL, EXAMPLE_CODE } from '../../common/constants/swagger-example.constant';
 
 export class ConfirmDto {
     @ApiProperty({ example: EXAMPLE_EMAIL })
     @IsEmail()
-    email: string;
+    public readonly email: string;
 
     @ApiProperty({ minLength: 4, maxLength: 4, example: EXAMPLE_CODE })
-    @IsString()
+    @IsNumberString()
     @Length(4, 4)
-    code: string;
+    public readonly code: string;
 }
