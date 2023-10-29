d4350f0400e106e4443c8fdc406f3317643c26f9
Ilya Stralkouski <i.stralkouski@a1.by>
2023-10-29 18:54:35 +0300

    create todo for swagger

--- src/swagger/swagger-setup.swagger.ts
+++ src/swagger/swagger-setup.swagger.ts
@@ -2,6 +2,7 @@ import type { INestApplication } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerSetup = (application: INestApplication) => {
+    // todo: add host
     const config = new DocumentBuilder()
         .setTitle('Chat Api')
         .setDescription('Created by Ilya Strelkovskiy')
